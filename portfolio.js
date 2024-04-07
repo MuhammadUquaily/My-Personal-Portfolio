b73d929b7da3022b0925d1f938ce72dccbb06222
Muhammad Uquaily <muqu2010@gmail.com>
2024-04-07 21:28:13 +0500

    "Modified portofolio.html and porfolio.js"

--- portfolio.js
+++ portfolio.js
@@ -2,9 +2,9 @@ const lccPara = document.getElementById('lcc-paragraph')
 const tsunamiPara = document.getElementById('tsunami-coffee-paragraph').hidden = true
 const storyPara = document.getElementById('story-paragraph').hidden = true
 
-document.getElementById('lcc-paragraph').hidden = true
-document.getElementById('tsunami-coffee-paragraph').hidden = true
-document.getElementById('story-paragraph').hidden = true
+lccPara.hidden = true
+tsunamiPara.hidden = true
+storyPara.hidden = true
 
 let lccHeading = document.getElementById('lcc-heading')
 let tsunamiHeading = document.getElementById('tsunami-coffee-heading')
@@ -14,6 +14,6 @@ function appear(para) {
     para.hidden = false
 }
 
-lccHeading.onclick = appear(lccPara)
-tsunamiHeading.onclick = appear(tsunamiPara)
-storyHeading.onclick = appear(storyPara)
+lccHeading.onclick = () => appear(lccPara)
+tsunamiHeading.onclick = () => appear(tsunamiPara)
+storyHeading.onclick = () => appear(storyPara)
