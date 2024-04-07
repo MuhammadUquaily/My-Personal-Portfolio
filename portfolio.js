ed7daef5ae075ac6b043998439dce043cf4c2b6e
Muhammad Uquaily <muqu2010@gmail.com>
2024-04-07 21:43:53 +0500

    "Corrected html and javascript"

--- portfolio.js
+++ portfolio.js
@@ -1,19 +1,19 @@
-const lccPara = document.getElementById('lcc-paragraph')
-const tsunamiPara = document.getElementById('tsunami-coffee-paragraph').hidden = true
-const storyPara = document.getElementById('story-paragraph').hidden = true
+const lccPara = document.getElementById('lcc-paragraph');
+const tsunamiPara = document.getElementById('tsunami-coffee-paragraph');
+const storyPara = document.getElementById('story-paragraph');
 
-lccPara.hidden = true
-tsunamiPara.hidden = true
-storyPara.hidden = true
+lccPara.hidden = true;
+tsunamiPara.hidden = true;
+storyPara.hidden = true;
 
-let lccHeading = document.getElementById('lcc-heading')
-let tsunamiHeading = document.getElementById('tsunami-coffee-heading')
-let storyHeading = document.getElementById('story-heading')
+let lccHeading = document.getElementById('lcc-heading');
+let tsunamiHeading = document.getElementById('tsunami-coffee-heading');
+let storyHeading = document.getElementById('story-heading');
 
 function appear(para) {
-    para.hidden = false
+    para.hidden = !para.hidden; 
 }
 
-lccHeading.onclick = () => appear(lccPara)
-tsunamiHeading.onclick = () => appear(tsunamiPara)
-storyHeading.onclick = () => appear(storyPara)
+lccHeading.onclick = () => appear(lccPara);
+tsunamiHeading.onclick = () => appear(tsunamiPara);
+storyHeading.onclick = () => appear(storyPara);
